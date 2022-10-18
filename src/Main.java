fff1c9bb65904c57a89cea6cc65c0a73ef643f0e
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-10-18 12:27:16 +0300

    MyTargetRefactor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        target(arr);
+    }
+
+    public static void target(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 arr[i] += 1;
