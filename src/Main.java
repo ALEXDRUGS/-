aed776c1fdf69222a77630a0b11080e52e05d5f9
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-10-19 16:12:16 +0300

    MyTargetRefactor2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        target(arr);
+        System.out.println(Arrays.toString(target(arr)));
     }
 
-    public static void target(int[] arr) {
+    public static int[] target(int[] arr) {
+        int[] gapArr = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 arr[i] += 1;
-            }
-            if (i > 1 && i < 7) {
-                System.out.print(arr[i]);
+                gapArr = Arrays.copyOfRange(arr, 2, 7);
             }
         }
+        return gapArr;
     }
 }
